15ff3cb2ec25bdc9d603c599d28a4a8aa91b440a
zhaocc <2122446868@qq.com>
2024-05-12 20:55:44 +0800

    feat:前端快速入门

---
+++ Web/frontend-quick-start/demo-04-vue/demo-vue3/src/components/From.vue
@@ -0,0 +1,75 @@
+<script setup>
+let name="张三";
+let consentCheck=false;
+let interest=['足球','篮球'];
+let sex='男';
+</script>
+
+<template>
+  <div style="display: flex;">
+    <div style="border: 1px solid black;width: 300px">
+      <form>
+        <h1>表单绑定</h1>
+        <p style="background-color: azure"><label>姓名(文本框)：</label><input v-model="name"/></p>
+        <p style="background-color: azure"><label>同意协议(checkbox)：</label>
+          <input type="checkbox" v-model="consentCheck"/>
+        </p>
+        <p style="background-color: azure">
+          <label>兴趣(多选框)：</label><br/>
+          <label><input type="checkbox" value="足球" v-model="interest"/>足球</label>
+          <label><input type="checkbox" value="篮球" v-model="interest" />篮球</label>
+          <label><input type="checkbox" value="羽毛球" v-model="interest" />羽毛球</label>
+          <label><input type="checkbox" value="乒乓球" v-model="interest" />乒乓球</label>
+        </p>
+        <p style="background-color: azure">
+          <label>性别(单选框)：</label>
+          <label><input type="radio" name="sex" value="男" v-model="sex">男</label>
+          <label><input type="radio" name="sex" value="女" v-model="sex">女</label>
+        </p>
+        <p style="background-color: azure">
+          <label>学历(单选下拉列表)：</label>
+          <select>
+            <option disabled value="">选择学历</option>
+            <option>小学</option>
+            <option>初中</option>
+            <option>高中</option>
+            <option>大学</option>
+          </select>
+        </p>
+        <p style="background-color: azure">
+          <label>课程(多选下拉列表)：</label>
+          <br/>
+          <select multiple>
+            <option disabled value="">选择课程</option>
+            <option>语文</option>
+            <option>数学</option>
+            <option>英语</option>
+            <option>道法</option>
+          </select>
+        </p>
+      </form>
+    </div>
+    <div style="border: 1px solid blue;width: 200px">
+      <h1>结果预览</h1>
+      <p style="background-color: azure"><label>姓名：</label></p>
+      <p style="background-color: azure"><label>同意协议：</label>
+      </p>
+      <p style="background-color: azure">
+        <label>兴趣：</label>
+      </p>
+      <p style="background-color: azure">
+        <label>性别：</label>
+      </p>
+      <p style="background-color: azure">
+        <label>学历：</label>
+      </p>
+      <p style="background-color: azure">
+        <label>课程：</label>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- Web/frontend-quick-start/demo-04-vue/demo-vue3/src/components/HelloWorld.vue
+++ Web/frontend-quick-start/demo-04-vue/demo-vue3/src/components/HelloWorld.vue
@@ -1,40 +1,82 @@
 <script setup>
-import { ref } from 'vue'
+import {computed, reactive, ref} from "vue";
 
-defineProps({
-  msg: String,
-})
+let name ="张三";
+let age=20;
+const count= ref(0);
+const obj =ref(
+    {
+      netsted:{count:0},
+      arr:['foo','bar']
+    }
+);
 
-const count = ref(0)
+const totalPrice =computed(()=>{
+  return car.price * car.num - car.coupon*100
+});
+
+const state =reactive({count:0});
+
+let car=reactive({
+  brand:"奔驰",
+  price:100,
+  coupon:0,
+  num:0
+});
+
+function buy(){
+  alert("购买成功");
+}
+
+function increment(){
+  count.value++;
+}
+function mutateDeeply(){
+  obj.value.netsted.count++;
+  obj.value.arr.push('baz');
+}
+
+function  addNum(){
+  car.num++;
+
+}
+function changePrice(){
+  car.price++;
+
+}
+function getCoupon(){
+
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <p> name: {{name}} </p>
+  <p> age: {{age}} </p>
+  <div style="border: 3px solid red">
+    <p>品牌：{{car.brand}}</p>
+    <p>价格：{{car.price}}</p>
   </div>
+  <button v-on:click="buy">购买</button>
+  <button @click="buy">购买</button>
+  <button @click="increment"> {{ count }}</button>
+  <button @click="mutateDeeply"> {{ obj.netsted.count }}</button>
+  <button @click="state.count++">
+    {{ state.count }}
+  </button>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+
+  <div class="car">
+    <h2>优惠券：{{ car.coupon }} 张</h2>
+    <h2>数量：{{ car.brand }}</h2>
+    <h2>单价：{{ car.price }}</h2>
+    <h1>总价：{{totalPrice}}</h1>
+    <button @click="getCoupon">获取优惠</button>
+    <button @click="addNum">加量</button>
+    <button @click="changePrice">加价</button>
+  </div>
+  
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
